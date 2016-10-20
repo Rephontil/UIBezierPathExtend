95de771dbb7c5bf980a4e464ded21e809eb4dac4
ZhouYong <Tan@mac-mini.local>
2016-10-20 18:06:54 +0800

    开始富文本的编辑

---
+++ UIBezierPathDeepResearch/UIBezierPathDeepResearch/ZYDrawTextAndPictureView.h
@@ -0,0 +1,13 @@
+//
+//  ZYDrawTextAndPictureView.h
+//  UIBezierPathDeepResearch
+//
+//  Created by ZhouYong on 16/10/20.
+//  Copyright © 2016年 Rephontil/Yong Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYDrawTextAndPictureView : UIView
+
+@end
---
+++ UIBezierPathDeepResearch/UIBezierPathDeepResearch/ZYDrawTextAndPictureView.m
@@ -0,0 +1,30 @@
+//
+//  ZYDrawTextAndPictureView.m
+//  UIBezierPathDeepResearch
+//
+//  Created by ZhouYong on 16/10/20.
+//  Copyright © 2016年 Rephontil/Yong Zhou. All rights reserved.
+//
+
+#import "ZYDrawTextAndPictureView.h"
+
+@implementation ZYDrawTextAndPictureView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    NSString *str = @"Only override drawRect: if you perform custom drawing.";
+    
+    NSMutableDictionary *attributeDic = [[NSMutableDictionary alloc] initWithCapacity:0];
+    attributeDic[@"NSFontAttributeName"] = [UIFont systemFontOfSize:30];
+    
+    
+    
+    [str drawInRect:rect withAttributes:attributeDic];
+    
+    
+}
+
+
+@end
