76ed27c3ee126cd57e6e9be6e7b3b561d91a7888
ZhouYong <1914701375@qq.com>
2016-10-21 08:14:54 +0800

    裁剪技术

--- UIBezierPathDeepResearch/UIBezierPathDeepResearch/ZYDrawTextAndPictureView.m
+++ UIBezierPathDeepResearch/UIBezierPathDeepResearch/ZYDrawTextAndPictureView.m
@@ -17,7 +17,7 @@
     NSString *str = @"Only override drawRect: if you perform custom drawing.";
     
     NSMutableDictionary *attributeDic = [[NSMutableDictionary alloc] initWithCapacity:0];
-    attributeDic[NSFontAttributeName] = [UIFont systemFontOfSize:15];
+    attributeDic[NSFontAttributeName] = [UIFont systemFontOfSize:20];
     attributeDic[NSForegroundColorAttributeName] = [UIColor yellowColor];
     attributeDic[NSStrokeWidthAttributeName] = @3;
     
@@ -26,9 +26,16 @@
     shade.shadowOffset = CGSizeMake(2, 10);
     attributeDic[NSShadowAttributeName] = shade;
 
+
     attributeDic[NSStrokeColorAttributeName] = [UIColor greenColor];
     [str drawInRect:rect withAttributes:attributeDic];
-    NSLog(@"nnnnnnnn");
+
+    //裁剪,裁剪的步骤一定要放在渲染的步骤之前进行.
+    UIRectClip(CGRectMake(40,40, 100, 50));
+    UIImage *image = [UIImage imageNamed:@"mile_20_icon"];
+//    [image drawInRect:rect];
+    [image drawAsPatternInRect:rect];
+
     
 }
 
